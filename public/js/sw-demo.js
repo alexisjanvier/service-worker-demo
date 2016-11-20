6fd028e0606c783f32681d72582a9ee8633a1ae7
Alexis Janvier <contact@alexisjanvier.net>
2016-11-20 18:16:18 +0100

    Service worker level 0

--- public/js/sw-demo.js
+++ public/js/sw-demo.js
@@ -1,2 +1,32 @@
-var ready = document.getElementById("demo-ready");
-ready.classList.remove("hidden");
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('service-worker.js', { scope: '/' });
+    mainMessage('notify', 'A very simple case');
+} else {
+    mainMessage('alert', 'You browser do not support Service Worker');
+}
+
+function mainMessage(type, msg) {
+    var ready = document.getElementById('main-message');
+    switch (type) {
+    case 'alert':
+        ready.classList.remove('green');
+        ready.classList.add('red');
+        break;
+    case 'notify':
+        ready.classList.remove('red');
+        ready.classList.add('green');
+        break;
+    default:
+        console.error(type + ' is not a valid type for a main message');
+        ready.classList.remove('red');
+        ready.classList.remove('green');
+        ready.innerText = '';
+        ready.classList.add('hidden');
+        return false;
+    }
+
+    ready.innerText = msg;
+    ready.classList.remove('hidden');
+
+    return true;
+}
---
+++ public/service-worker.js
@@ -0,0 +1,5 @@
+var version = '00';
+
+self.addEventListener('install', event => {
+  console.log('First service worker log')
+});
