50cb1d35ed5c6da870d596277f1cf35ba6cea898
Alexis Janvier <contact@alexisjanvier.net>
2017-02-24 15:14:45 +0100

    First minimal offline version

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,15 +1,45 @@
-var version = '04';
+var CACHE_NAME = 'sw-demo_1';
 
-self.addEventListener('install', event => {
-  console.log('Log from event "INSTALL" in service worker version ' + version);
-  return self.skipWaiting();
+var urlsToCache = [
+    '/index.html',
+    '/playlist.html',
+    '/css/normalize.css',
+    '/css/sw-demo.css',
+    '/images/catWorker.jpg',
+    '/images/logo.png',
+    '/covers/avecpasdcasque.jpg',
+    '/covers/ryleywalker.jpg',
+    '/covers/xeniaRubinos.jpg',
+    '/js/sw-demo.js',
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+        .then(cache => cache.addAll(urlsToCache))
+        .then(() => self.skipWaiting())
+    );
 });
 
-self.addEventListener('fetch', event => {
-  console.log('Log from event "FETCH" in service worker version ' + version);
+
+self.addEventListener('activate', function activator (event) {
+    event.waitUntil(
+        caches.keys()
+            .then(keys => {
+                return Promise.all(keys
+                    .filter(key => key !== CACHE_NAME)
+                    .map(key => caches.delete(key))
+                );
+            })
+            .then(() => self.clients.claim())
+    );
 });
 
-self.addEventListener('activate', event => {
-  console.log('Log from event "ACTIVATE" in service worker version ' + version);
-  return self.clients.claim();
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        fetch(event.request)
+            .catch(() => {
+                return caches.match(event.request);
+            })
+    );
 });
